5f5325b8c98155d4af2f384f3e960c2e51af697b
naosuke <info@naosuke.me>
2018-05-23 20:48:45 +0900

    [wip]

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -1,43 +1,58 @@
 <template>
+<div>
+  <div class="row">
+    <div class="col s10 m11">
+      <input class="form-control" placeholder="add your task!">
+    </div>
+    <div class="col s2 m1">
+      <div class="btn-floating waves-effect waves-light red">
+        <i class="material-icons">add</i>
+      </div>
+    </div>
+  </div>
   <div>
-    <div class="row">
-      <div class="col s10 m11">
-        <input class="form-control" placeholder="add your task!">
-      </div>
-      <div class="col s2 m1">
-        <div class="btn-floating waves-effect waves-light red">
-          <i class="material-icons">add</i>
-        </div>
-      </div>
-    </div>
-    <div>
-      <ul class="collection">
-        <li id="row_task_1" class="collection-item">
-          <input type="checkbox" id="task_1">
-          <label for="task_1">Sample Task</label>
-        </li>
-        <li id="row_task_2" class="collection-item">
-          <input type="checkbox" id="task_2">
-          <label for="task_2">Sample Task</label>
-        </li>
-        <li id="row_task_3" class="collection-item">
-          <input type="checkbox" id="task_3">
-          <label for="task_3">Sample Task</label>
-        </li>
-      </ul>
-    </div>
+    <ul class="collection">
+      <li v-for="task in tasks" v-if="!task.is_done" v-bind:id="'row_task_' + task.id" class="collection-item">
+        <input type="checkbox" v-bind:id="'task_' + task.id">
+        <label v-bind:for="'task_' + task.id">{{ task.name }}</label>
+      </li>
+    </ul>
+  </div>
     <div class="btn">Display finished tasks</div>
     <div id="finished-tasks" class="display_none">
       <ul class="collection">
-        <li id="row_task_4" class="collection-item">
-          <input type="checkbox" id="'task_4" checked="checked" />
-          <label v-bind:for="task_4" class="line-through">Done Task</label>
-        </li>
-        <li id="row_task_5" class="collection-item">
-          <input type="checkbox" id="'task_5" checked="checked" />
-          <label v-bind:for="task_5" class="line-through">Done Task</label>
-        </li>
+      <li v-for="task in tasks" v-if="task.is_done" v-bind:id="'row_task_' + task.id" class="collection-item">
+        <input type="checkbox" v-bind:id="'task_' + task.id" checked="checked">
+        <label v-bind:for="'task_' + task.id">{{ task.name }}</label>
+      </li>
       </ul>
     </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data: () => {
+        return {
+            tasks: [],
+            newTask: ''
+        }
+    },
+    mounted: () => {
+        this.fetchTasks();
+    },
+    methods: {
+        fetchTasks: () => {
+            axios.get('/api/tasks').then((response) => {
+                for ( task of response.data.tasks ) {
+                    this.tasks.push(task)
+                }
+            }, (error) => {
+                console.log(error)
+            });
+        },
+    },
+  }
+</script>
